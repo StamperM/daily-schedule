74493d580636f5dc85041e44f5a27f686ac56a08
melissa <90412072+StamperM@users.noreply.github.com>
2023-01-06 23:33:14 -0500

    adding dayjs

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+var today = dayjs([])
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+$('#today').text(today.format('MMM D, YYYY'));
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
