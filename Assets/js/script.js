169d7dfc9ddd0ee6bca2c197a9c2f1ece45e2ac0
Melissa <90412072+StamperM@users.noreply.github.com>
2023-01-13 18:58:52 -0500

    finished JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,26 +3,16 @@ var toDoDescription = $(".description");
 var button = $(".saveBtn");
 var thingsToDo = {};
 var descriptionValue ;
+var todaysDate = $("#currentDay").text(today.format("MMM D, YYYY"));
 
-var timeBlock = dayjs().set("hour");
 $(document).ready(function () {
   updateTimeSlots();
 });
 
-
-var todaysDate = $("#currentDay").text(today.format("MMM D, YYYY"));
-
-
 function updateTimeSlots() {
-  // get current hour
-  var currentTime = dayjs().format("H");
-  // select timebox class
+  var currentTime = parseInt(dayjs().format("H"));
   $(".time-block").each(function(){
- // get ID and split hour away from ID
- var timeblockHour = $(this).attr('id').split("-")[1];
- // get difference betwee current hour and split hour 
-
- // assign past present and future
+ var timeblockHour = parseInt($(this).attr('id').split("-")[1]);
 
   if (timeblockHour < currentTime) {
     $(this).addClass("past");
@@ -33,28 +23,17 @@ function updateTimeSlots() {
   }
 })
 }
- 
-
-
-
-
-// 
-
-
-
 
 $(function () {
   $(".saveBtn").click(function (event){
     console.log("save");
-// on save button read value and time
  event.preventDefault();
   descriptionValue = $(this).siblings(".description").val();
  var hourId = $(this).parent().attr('id');
  var splitHourId = hourId.split('-')[1];
  var currentUpdatedToDo = JSON.parse(localStorage.getItem("timeblock")|| "{}");
 currentUpdatedToDo[splitHourId]=descriptionValue;
-localStorage.setItem('timeblock',JSON.stringify( currentUpdatedToDo))
-    
+localStorage.setItem('timeblock',JSON.stringify( currentUpdatedToDo))   
 }
 )
 })
